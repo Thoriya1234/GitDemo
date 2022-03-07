a84d3e4e3045e3e82e1bd23279685404a9a43a8d
Thoriya1234 <thoriyamona@gmail.com>
2022-03-07 16:53:52 +0530

    first commit

---
+++ src/AppiumTest.java
@@ -0,0 +1,23 @@
+package qaacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+
+
+public class AppiumTest{
+	
+	@Test
+	public void postjira()
+	{
+		System.out.println("postjira");
+		
+	}
+	
+	@Test
+	public void delettwitter()
+	{
+		System.out.println("deletetwitter");
+		
+	}
+}
+
